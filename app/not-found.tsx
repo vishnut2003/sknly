aaa1ee2d95587bb9bd2ec108fb38136a9b0ef075
vishnu <iam.vishnu.bhaskar@gmail.com>
2026-03-04 17:11:47 +0530

    Completed 404 Page;

---
+++ app/not-found.tsx
@@ -0,0 +1,49 @@
+import InnerPagesLayout from "@/layouts/inner-pages-layout"
+import ErrorTextImage from "./404-page-assets/error-text.png";
+import SadFaceImage from "./404-page-assets/sad-face.png";
+import GraphicImage from "./404-page-assets/graphics.png";
+import DefaultSection from "@/layouts/default-section";
+import Image from "next/image";
+
+const NotFound = () => {
+  return (
+    <InnerPagesLayout>
+        <DefaultSection
+            outerClassName="py-10 bg-[#FCDFE2]"
+            className="flex flex-col gap-3 items-center justify-center min-h-100"
+        >
+            <div
+                className="flex items-center justify-center gap-5"
+            >
+                <Image
+                    alt="Error Text"
+                    src={ErrorTextImage}
+                    className="w-110"
+                />
+                <Image
+                    alt="Sad Face"
+                    src={SadFaceImage}
+                    className="w-18"
+                />
+            </div>
+
+            <p
+                className="text-center text-lg text-[#BA131C]"
+            >Oops! This page melted. Let's get you glowing again in some time✨</p>
+        </DefaultSection>
+
+        <div
+            className="pb-10"
+        >
+            <Image
+                alt="Graphics Image"
+                src={GraphicImage}
+                className="w-full"
+            />
+        </div>
+
+    </InnerPagesLayout>
+  )
+}
+
+export default NotFound

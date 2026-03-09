9182e66a76aed2dfb3772a56f754c82b4dac90e7
vishnu <iam.vishnu.bhaskar@gmail.com>
2026-03-09 16:30:07 +0530

    Minor Changes;

--- app/(components)/join-sknly-section/index.tsx
+++ app/(components)/join-sknly-section/index.tsx
@@ -3,6 +3,7 @@ import SectionBG from "./assets/bg-mountain.png";
 import SportsImage from "./assets/sports-image.png";
 import JoinClubForm from "@/components/ui-elements/join-club-form";
 import DefaultSection from "@/layouts/default-section";
+import LogoText from "./assets/logo-text.png";
 
 const JoinSknlySection = ({ hideBgImage, className }: {
     hideBgImage?: boolean,
@@ -20,25 +21,51 @@ const JoinSknlySection = ({ hideBgImage, className }: {
                     className="flex items-center flex-col-reverse md:flex-row"
                 >
                     <div
-                        className="w-full"
+                        className="w-full flex items-end justify-between"
                     >
                         <Image
                             alt="Sports"
                             src={SportsImage}
-                            className="w-full h-full object-cover"
+                            className="w-1/2 md:w-full h-full object-cover"
                         />
+
+                        <div
+                            className="md:hidden p-10"
+                        >
+                            <Image
+                                alt="Logo"
+                                src={LogoText}
+                                className="w-40 mx-auto"
+                            />
+                        </div>
+
                     </div>
                     <div
-                        className="w-full text-[#BA131C] flex flex-col justify-center items-stretch space-y-4"
+                        className="w-full text-[#BA131C] flex flex-col justify-center items-stretch space-y-10"
                     >
-                        <h2
-                            className="font-glamour text-4xl text-[#BA131C]"
-                        >Join the sknly. club!</h2>
-                        <p
-                            className="text-[#BA131C]"
-                        >Enjoy free shipping on your first order and unlock more juicy perks inside 💕</p>
+                        <div
+                            className="space-y-4"
+                        >
+                            <h2
+                                className="font-glamour text-4xl text-[#BA131C]"
+                            >Join the sknly. club!</h2>
+                            <p
+                                className="text-[#BA131C]"
+                            >Enjoy free shipping on your first order and unlock more juicy perks inside 💕</p>
 
-                        <JoinClubForm />
+                            <JoinClubForm />
+                        </div>
+
+                        <div
+                            className="hidden md:block"
+                        >
+                            <Image
+                                alt="Logo"
+                                src={LogoText}
+                                className="w-40 mx-auto"
+                            />
+                        </div>
+
                     </div>
                 </DefaultSection>
             </div>

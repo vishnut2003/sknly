de9c1fea68dde8d25f631ab85f8b456d672a475d
vishnu <iam.vishnu.bhaskar@gmail.com>
2026-03-13 05:01:10 +0530

    Connect before making db request;

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -3,6 +3,7 @@ import { PropsWithChildren } from "react";
 import { authOptions } from "../api/auth/[...nextauth]/authOptions";
 import { notFound } from "next/navigation";
 import UserModel, { UsersModelInterface } from "@/models/user";
+import { dbConnect } from "@/config/database";
 
 async function AdminDashboardRootLayout({
     children,
@@ -14,6 +15,7 @@ async function AdminDashboardRootLayout({
         notFound();
     }
 
+    await dbConnect();
     const user = await UserModel.findById(session.user.id) as UsersModelInterface | null;
 
     if (!user?.role || user.role !== "admin") {

ee353c5a06a8a0ffd8c783941c2427384d4bd429
vishnu <iam.vishnu.bhaskar@gmail.com>
2026-03-12 13:35:00 +0530

    disable redirect to login page from home page;

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -21,7 +21,7 @@ export default async function Home() {
       {/* <BundleMoreSectionTwo /> */}
       <WippedServeSection />
 
-      <RedirectToLoginPageOneTime/>
+      {/* <RedirectToLoginPageOneTime/> */}
       {/* <EspressoMoussePopup/> */}
 
     </HomeLayout>
